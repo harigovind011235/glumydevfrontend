dd9d81aea3e06ffb8d5d8dc23ca39d827d30b2c6
Yaduk035 <yaduk946@gmail.com>
2023-05-05 23:30:36 +0530

    fourth commit

--- src/Routes.js
+++ src/Routes.js
@@ -49,31 +49,16 @@ const routes = createBrowserRouter([
       </Suspense>
     ),
   },
-<<<<<<< HEAD
-  {
-    path: "/adminjobsdashboard",
-    element: (
-      <Suspense fallback={<Loader></Loader>}>
-        <AdminJobsDashboard/>
-      </Suspense>
-    ),
-  },
-  {
-    path: "/adminjobupdate",
-    element: (
-      <Suspense fallback={<Loader></Loader>}>
-        <AdminJobCard/>
-=======
 
   {
     path: "/jobpostings",
     element: (
       <Suspense fallback={<Loader></Loader>}>
         <JobScreen/>
->>>>>>> 97664824ce2f572aef94c53ecbb69831415fa111
       </Suspense>
     ),
   },
+  
 
 ]);
 
