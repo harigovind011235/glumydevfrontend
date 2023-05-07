2ae88359e412cf10fa8fa4cc5048dba30da5aaff
nehamary25 <120901568+nehamarybaby@users.noreply.github.com>
2023-05-08 01:47:07 +0530

    Neha chnages

---
+++ src/components/SearchResult.js
@@ -0,0 +1,6 @@
+import { Link as RouterLink } from 'react-router-dom';
+
+// inside your render function
+<RouterLink to="/search-results">
+  <Button variant="outline-dark">Search</Button>
+</RouterLink>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -15,17 +15,20 @@ function HomeScreen() {
       </h2>
       <Container>
         <Row style={{ marginTop: "40px" }}>
-          <Col className="lg-4">
-            <HomeCard />
-          </Col>
-          <Col className="lg-3">
-            <HomeCard />
-          </Col>
-          <Col className="lg-3">
-            <HomeCard />
-          </Col>
-          <Col className="lg-3">
-            <HomeCard />
+          <Col className="d-flex justify-content-center">
+            <HomeCard
+              title="Google"
+              description="Google is a multinational technology company that specializes in Internet-related services and products."
+            />
+            <HomeCard
+              title="Microsoft"
+              description="Microsoft is an American multinational technology company that develops, licenses, and sells computer software, consumer electronics, and personal computers."
+              />
+            <HomeCard
+              title="Amazon"
+              description="Amazon is an American multinational technology company that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence."
+              />
+              
           </Col>
         </Row>
       </Container>
@@ -34,22 +37,24 @@ function HomeScreen() {
       </h2>
       <Container>
         <Row style={{ marginTop: "40px" }}>
-          <Col className="lg-4">
-            <HomeCard />
-          </Col>
-          <Col className="lg-3">
-            <HomeCard />
-          </Col>
-          <Col className="lg-3">
-            <HomeCard />
-          </Col>
-          <Col className="lg-3">
-            <HomeCard />
+          <Col className="d-flex justify-content-center">
+            <HomeCard
+              title="Software Engineer"
+              description="We're seeking a talented software engineer to join our team and help us build innovative software solutions. The ideal candidate should have experience with programming languages like Java, Python, or JavaScript."
+            />
+            <HomeCard
+              title="Product Manager"
+              description="We're looking for an experienced product manager to join our team and help us build amazing products that our customers love. The ideal candidate should have a strong understanding of customer needs, as well as experience with product development and management."
+            />
+            <HomeCard
+              title="Data Scientist"
+              description="We're seeking a data scientist to join our team and help us uncover insights from our data. The ideal candidate should have experience with statistical analysis, data visualization, and machine learning, as well as proficiency in Python or R."
+            />
           </Col>
         </Row>
       </Container>
       <Container style={{ marginTop: "60px" }}>
-        <Footer/>
+        <Footer />
       </Container>
     </>
   );
