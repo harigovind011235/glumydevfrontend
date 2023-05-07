a119596d8655ae00c34b8939079773061bebe1a4
Yaduk035 <yaduk946@gmail.com>
2023-05-07 22:10:18 +0530

    removed routes, package.json etc from gitignore

---
+++ src/Routes.js
@@ -0,0 +1,78 @@
+import React, { lazy, Suspense } from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Loader from "./components/Loader";
+import JobCreation from "./screens/JobCreation";
+
+const HomeScreen = lazy(() => import("./screens/HomeScreen"));
+const RegisterScreen = lazy(() => import("./screens/RegisterScreen"));
+const LoginScreen = lazy(() => import("./screens/LoginScreen"));
+const OurCompanyScreen = lazy(() => import("./screens/OurCompanyScreen"));
+const Aboutusscreen = lazy(() => import("./screens/Aboutus"));
+const AdminJobscreen = lazy(() => import("./screens/AdminJobscreen"));
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <HomeScreen />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <RegisterScreen />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/login",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <LoginScreen />
+      </Suspense>
+    ),
+  },
+  {
+    path: "/ourcompanies",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <OurCompanyScreen/>
+      </Suspense>
+    ),
+  },
+  {
+    path: "/jobcreation",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <JobCreation/>
+      </Suspense>
+    ),
+  },
+
+  {
+    path: "/aboutus",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <Aboutusscreen/>
+      </Suspense>
+    ),
+  },
+
+  {
+    path: "/
+    ",
+    element: (
+      <Suspense fallback={<Loader></Loader>}>
+        <AdminJobscreen/>
+      </Suspense>
+    ),
+  },
+
+
+]);
+
+export default routes;
